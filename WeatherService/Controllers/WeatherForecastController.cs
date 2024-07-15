30125e30afc2006e09db8ebc94372c7735a884f6
Avik Manna <avikmanna@Aviks-Laptop.local>
2024-07-15 03:33:35 +0200

    small update

--- WeatherService/Controllers/WeatherForecastController.cs
+++ WeatherService/Controllers/WeatherForecastController.cs
@@ -22,14 +22,9 @@ public class WeatherForecastController : ControllerBase
     /// <param name="date">Date</param>
     /// <returns>Forecst detail</returns>
     [HttpGet("getforecast")]
-    public ActionResult GetForecast(string location, DateTime date)
+    public ActionResult<ForecastResponse> GetForecast(string location, DateTime date)
     {
-        if (!string.IsNullOrEmpty(location))
-        {
-            return Ok(this._forecastService.GetForecast(location, date));
-        }
-        else
-            return BadRequest();
+        return Ok(this._forecastService.GetForecast(location, date));
     }
 
     /// <summary>
@@ -40,13 +35,7 @@ public class WeatherForecastController : ControllerBase
     [HttpPost("addforecast")]
     public ActionResult<Status> AddForecast(Forecast forecast)
     {
-        if (forecast != null)
-        {
-            Status s = this._forecastService.AddForecast(forecast);
-            return Ok(s);
-        }
-        else
-            return BadRequest();
+        return Ok(this._forecastService.AddForecast(forecast));
     }
 }
 
